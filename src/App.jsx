87eb2ca54c240533e654691fc39a4f1cf03cd84b
itsvikash93 <vikashkumar827070@gmail.com>
2025-04-20 17:46:04 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,81 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Page1 from "./Components/Page1";
 import Page2 from "./Components/Page2";
 import Page3 from "./Components/Page3";
 import Page4 from "./Components/Page4";
 import Page5 from "./Components/Page5";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import ReactLenis, { useLenis } from "lenis/react";
 
 const App = () => {
-  const scrollContainerRef = useRef(null);
+  const lenis = useLenis();
+  gsap.registerPlugin(ScrollTrigger);
 
-  useGSAP(() => {}, []);
+  const scrollContainerRef = useRef(null);
+  const parent = useRef(null);
+
+  useEffect(() => {
+    if (!parent.current || !lenis) return;
+
+    ScrollTrigger.scrollerProxy(parent.current, {
+      scrollTop(value) {
+        if (arguments.length) {
+          lenis.scrollTo(value);
+        }
+        return lenis.scroll;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      pinType: parent.current.style.transform ? "transform" : "fixed",
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.defaults({
+      scroller: parent.current,
+    });
+
+    ScrollTrigger.refresh();
+  }, [lenis]);
+  useGSAP(() => {
+    gsap.to(scrollContainerRef.current, {
+      x: "-100%",
+      ease: "none",
+      scrollTrigger: {
+        trigger: scrollContainerRef.current,
+        start: "top top",
+        end: "top -100%",
+        scroller: parent.current,
+        pin: true,
+        scrub: true,
+        markers: true,
+      },
+    });
+  }, []);
 
   return (
-    <div
-      ref={scrollContainerRef}
-      className={`w-screen h-screen md:flex`}
-      tabIndex={0}
-    >
-      <div className="w-full h-full flex flex-shrink-0">
-        <Page1 />
-        <Page2 />
-        <Page3 />
-        <Page4 />
-        <Page5 />
+    <ReactLenis root>
+      <div ref={parent} className={`relative w-full h-screen overflow-hidden`}>
+        <div
+          ref={scrollContainerRef}
+          className="w-[500vw] h-full flex flex-shrink-0"
+        >
+          <Page1 />
+          <Page2 />
+          <Page3 />
+          <Page4 />
+          <Page5 />
+        </div>
       </div>
-    </div>
+    </ReactLenis>
   );
 };
 
--- src/Components/Page1.jsx
+++ src/Components/Page1.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Page1 = () => {
   return (
-    <div className="flex-none w-screen h-screen bg-zinc-950 text-white text-xl">
+    <div className="flex-none w-[100vw] h-screen bg-zinc-950 text-white text-xl">
       <div className="flex items-center justify-center w-full h-screen ">
         Page1
-
       </div>
     </div>
   );
